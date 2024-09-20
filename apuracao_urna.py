444559aa06e5737bc4e72c07344f0479b6c5ed44
madebypissaldo <pissaldo.gabriel.bonfim@gmail.com>
2024-09-19 22:39:13 -0300

    update 0.1
    rodando sem a classe

--- apuracao_urna.py
+++ apuracao_urna.py
@@ -1,23 +1,15 @@
 import pickle
 import hashlib
 
-class Voto:
-    def __init__(self, candidate_id):
-        self.candidate_id = candidate_id
-
-    def __repr__(self):
-        return f"Voto(candidate_id={self.candidate_id})"
-
-    def to_string(self):
-        if self.candidate_id == 1:
-            return "Gabriel"
-        elif self.candidate_id == 2:
-            return "Joao"
-        elif self.candidate_id == 3:
-            return "Maria"
-        else:
-            return "Candidato desconhecido"
-
+def to_string(candidate_id):
+    if candidate_id == 1:
+        return "Gabriel"
+    elif candidate_id == 2:
+        return "Joao"
+    elif candidate_id == 3:
+        return "Maria"
+    else:
+        return "Candidato desconhecido"
 
 def carregar_planilha():
     try:
@@ -29,10 +21,8 @@ def carregar_planilha():
 def contar_votos(votos):
     vote_counts = {1: 0, 2: 0, 3: 0}
     for voto in votos:
-        vote_counts[voto.candidate_id] += 1
+        vote_counts[voto] += 1
     return vote_counts
-
-votos =  carregar_planilha()
 
 def gerar_hash(senha):
     return hashlib.sha256(senha.encode()).hexdigest()
@@ -44,6 +34,7 @@ hash_armazenado = gerar_hash("2209")
 
 acesso = input('digite a senha para acessar a  planilha: ')
 if verificar_senha(acesso, hash_armazenado):
+    votos = carregar_planilha()
     vote_counts = contar_votos(votos)
     print("Apuração dos votos:")
     print(f"Gabriel: {vote_counts[1]}")
